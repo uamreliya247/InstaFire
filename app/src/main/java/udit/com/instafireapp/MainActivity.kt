0e1770c2852a0438adb0b344a29f58d5481cacec
udit <uamreliya@gmail.com>
2020-04-07 18:21:30 +0530

    Profile Activity created.

--- app/src/main/java/udit/com/instafireapp/MainActivity.kt
+++ app/src/main/java/udit/com/instafireapp/MainActivity.kt
@@ -7,17 +7,20 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 import kotlinx.android.synthetic.main.activity_main.*
 import udit.com.instafireapp.adapters.PostAdapter
 import udit.com.instafireapp.models.Post
+import udit.com.instafireapp.models.User
 
 private const val TAG = "MainActivity"
+private const val EXTRA_USERNAME = "EXTRAUSERNAME"
 
-class MainActivity : AppCompatActivity() {
+open class MainActivity : AppCompatActivity() {
 
-
+    private var signedInUser: User? = null
     private lateinit var firebaseDb: FirebaseFirestore
     private lateinit var posts: MutableList<Post>
     private lateinit var postAdapter: PostAdapter
@@ -31,13 +34,29 @@ class MainActivity : AppCompatActivity() {
         rvPost.adapter = postAdapter
         rvPost.layoutManager = LinearLayoutManager(this)
 
-
         firebaseDb = FirebaseFirestore.getInstance()
 
-        val postReference = firebaseDb
+        firebaseDb.collection("users")
+            .document(FirebaseAuth.getInstance().currentUser?.uid as String)
+            .get()
+            .addOnSuccessListener { userSnapshot ->
+                signedInUser = userSnapshot.toObject(User::class.java)
+                Log.i(TAG, "Signed in user $signedInUser")
+            }
+            .addOnFailureListener { exception ->
+                Log.i(TAG, exception.localizedMessage)
+            }
+
+        var postReference = firebaseDb
             .collection("posts")
             .limit(20)
             .orderBy("creationTime", Query.Direction.DESCENDING)
+
+        val username = intent.getStringExtra(EXTRA_USERNAME)
+        if (username != null) {
+            supportActionBar?.title = username
+            postReference = postReference.whereEqualTo("user.username", username)
+        }
 
         postReference.addSnapshotListener { snapshot, exception ->
 
@@ -51,9 +70,6 @@ class MainActivity : AppCompatActivity() {
             posts.clear()
             posts.addAll(postList)
             postAdapter.notifyDataSetChanged()
-            for (post in postList) {
-                Log.i(TAG, "Posts $post")
-            }
         }
 
     }
@@ -65,7 +81,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.menuProfile) {
-            startActivity(Intent(this, ProfileActivity::class.java))
+            val intent = Intent(this, ProfileActivity::class.java)
+            intent.putExtra(EXTRA_USERNAME, signedInUser?.username)
+            startActivity(intent)
         }
 
         return super.onOptionsItemSelected(item)
--- app/src/main/java/udit/com/instafireapp/ProfileActivity.kt
+++ app/src/main/java/udit/com/instafireapp/ProfileActivity.kt
@@ -10,16 +10,12 @@ import com.google.firebase.auth.FirebaseAuth
 
 private const val TAG = "ProfileActivity"
 
-class ProfileActivity : AppCompatActivity() {
+class ProfileActivity : MainActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
-    }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_profile, menu)
-        return super.onCreateOptionsMenu(menu)
+        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
